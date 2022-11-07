eba091adb27d58829fbe7e1c32ec5fd690bbd8a6
Daniel Macharia <danielhuios@gmail.com>
2022-11-07 18:28:18 +0300

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicaitonController < Sinatra::Base
+
+    set :default_content_type, "application/json"
+
+    before do
+        response.headers['Access-Control-Allow-Origin'] = "*"
+    end
+
+    #enable CORS preflight requests
+    options "*" do
+        response.headers['Access-Control-Allow-Methods'] = "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    end
+
+end
